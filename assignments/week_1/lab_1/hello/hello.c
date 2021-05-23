83c0b176c0bce6e41c53747d8c43944ef414c2e9
HawksSpawn <67832524+HawksSpawn@users.noreply.github.com>
2021-05-23 15:58:46 +0200

    Add solution Lab 1 (Hello)

---
+++ assignments/week_1/lab_1/hello/hello.c
@@ -0,0 +1,10 @@
+// Print "hello, name", where 'name' is the user's name
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    string name = get_string("What is your name?\n");
+    printf("hello, %s\n", name);
+}
